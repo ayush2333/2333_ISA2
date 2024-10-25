fcaf0dcb3dde0cebfeb569ad118f5c4839e9920b
Ayush <144894664+ayush2333@users.noreply.github.com>
2024-10-25 15:30:06 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Name: Ayush , Roll No: 2333"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
